2431220a1a524a746dd24c7e8e008aa5f9b4f4bc
Hugo J <hjbello.wk@gmail.com>
2020-12-13 11:58:05 +0100

    new proj

--- New Unity Project/Assets/Bird.cs
+++ New Unity Project/Assets/Bird.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
 
+    Vector3 _initialPosition;
+
+    private void Awake() {
+        _initialPosition = transform.position;
+    }
+
+    private void Update() {
+        
+    }
+
     private void OnMouseDown() 
     {
         GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp() 
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnMouseDrag() {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
     }
 }
