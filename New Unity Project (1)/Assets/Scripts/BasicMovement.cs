1f5c328213e35c69149042351911c86aafe8790b
Hugo J <hjbello.wk@gmail.com>
2020-12-14 11:41:40 +0100

    clean up

--- New Unity Project (1)/Assets/Scripts/BasicMovement.cs
+++ New Unity Project (1)/Assets/Scripts/BasicMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,12 @@ public class BasicMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime;
+
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        Vector3 vertical = new Vector3(0.0f,Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + vertical * Time.deltaTime;
     }
 }
