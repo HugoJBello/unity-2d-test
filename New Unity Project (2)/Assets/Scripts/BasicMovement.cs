6a89ddca22f589f058688d9361cbfd5da32f27d5
Hugo J <hjbello.wk@gmail.com>
2021-01-04 19:48:13 +0100

    clean up

---
+++ New Unity Project (2)/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public bool isDragging;
+    public bool enters;
+
+    private void OnMouseUp() {
+        isDragging = false;
+
+    }
+
+    private void OnMouseDown() {
+        isDragging = true;
+    }
+
+    void Update()
+    {
+        if (isDragging) {
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(position);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        enters = true;
+        Debug.Log("entered");
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+                enters = true;
+                Debug.Log("stais");
+    }
+
+private void OnTriggerExit2D(Collider2D other) {
+                    enters = false;
+                    Debug.Log("leaves ");
+}
+
+}
---
+++ New Unity Project (2)/Assets/Scripts/DragArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragArea : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("entered");
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+                Debug.Log("staid");
+
+    }
+}
